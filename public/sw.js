1fc1caa91541794858ca1fa551c1d2254face163
anthony <universalmiusic@gmail.com>
2026-03-09 21:55:44 -0400

    Eliminar cache Service Worker definitivo

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,4 @@
-// Service Worker desactivado
 self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
-      .then(() => self.clients.claim())
-  );
-});
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
